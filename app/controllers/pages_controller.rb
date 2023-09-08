c23446611a9fb17027c28928504800ae4a67105c
Gary Smailes <garysmailes@gmail.com>
2023-09-08 14:42:02 +0100

    Add layout to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
     def index
       @page_title = 'Welcome' 
       @page_description = 'Home of high quality erotic fiction'
+      @featured_books = Book.order(created_at: :desc).limit(4)
+      @latest_posts = Post.published.order(created_at: :desc).limit(10)
     end
 
     def about
