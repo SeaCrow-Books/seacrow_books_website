6238edc1a4703b1204884ab9cc2761edec6374d2
Gary Smailes <garysmailes@gmail.com>
2023-11-04 10:17:54 +0000

    UYpdate tags

---
+++ app/policies/tag_policy.rb
@@ -0,0 +1,30 @@
+class TagPolicy < ApplicationPolicy
+
+    def index?
+        user&.admin? || user&.staff?
+    end
+  
+    def show?
+        user&.admin? || user&.staff?
+    end
+  
+    def create?
+        user&.admin? || user&.staff?
+    end
+  
+    def destroy?
+      user&.admin?
+    end
+  
+    def update?
+      user&.admin? || user&.staff?
+    end
+  
+    class Scope < Scope
+      def resolve
+        scope.all
+      end
+    end
+
+end
+  
