70d2ff67d842ed773ad5b3c33476ac90e20bf25c
Gary Smailes <garysmailes@gmail.com>
2024-07-21 13:33:33 +0100

    Add Brevo

--- app/models/email_capture.rb
+++ app/models/email_capture.rb
@@ -3,4 +3,13 @@ class EmailCapture < ApplicationRecord
     # Validations
     validates :email, presence: true
 
+    # Callbacks
+    after_create :add_to_brevo
+
+    private
+
+    def add_to_brevo
+        BrevoService.add_contact(email)
+    end
+
 end
